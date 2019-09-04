3d4c62cd0ea46e319bf38ecc7483490fe871052d
Allen Hsieh <allenhsiehhr@gmail.com>
2019-09-03 23:20:45 -0700

    replaced initial server data with lorem sentences

--- server/middlewares/api.js
+++ server/middlewares/api.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const data = ['hello', 'world', 'testing'];
+const data = [
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+  'Integer nec odio',
+  'Praesent libero',
+  'Sed cursus ante dapibus diam',
+  'Sed nisi',
+  'Nulla quis sem at nibh elementum imperdiet',
+  'Duis sagittis ipsum',
+  'Praesent mauris',
+  'Fusce nec tellus sed augue semper porta',
+  'Mauris massa',
+];
 
 router.get('/data', (req, res) => {
   res.send(data);
